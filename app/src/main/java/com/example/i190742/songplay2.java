2ec68be9e6621d2564b3b1c4f1ed4fb236113715
Beetavirusss <i190742@nu.edu.pk>
2022-09-18 22:16:35 +0500

    Done

---
+++ app/src/main/java/com/example/i190742/songplay2.java
@@ -0,0 +1,14 @@
+package com.example.i190742;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class songplay2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songplay2);
+    }
+}
